0c4b595286d5807719c80597820281fa55784bf1
mstfklc01 <m.kml.klc01@gmail.com>
2026-04-08 13:44:56 +0300

    Update Odev.c

--- Ödev/Odev.c
+++ Ödev/Odev.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int sayiVarMi(int dizi[], int istenen)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        if(dizi[i] == istenen)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
 int main() {
     int numbers[5] = {3,5,7,9,11};
     int sayi;
@@ -7,13 +20,13 @@ int main() {
     printf("Sayıyı giriniz: ");
     scanf("%d", &sayi);
     
-    for(int i = 0; i < 5; i++)
+    if(sayiVarMi(numbers,sayi) == 1)
     {
-        if(sayi == numbers[i])
-        {
-            printf("True");
-            break;
-        }
+        printf("True");
+    }
+    else
+    {
+        printf("False");
     }
 
     return 0;
